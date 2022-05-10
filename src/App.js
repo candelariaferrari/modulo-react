8419a2758dc03bf0079cdba2886b036d05290f65
candelaria ferrari <ferrari.candelaria@gmail.com>
2022-05-10 00:33:31 -0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
 import './assets/css/App.css';
-import Pelicula from './components/Pelicula'
+
+import Sidebar from './components/SideBar';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HOOOOLA REACT
-        </a>
-      </header>
-      <main>
-        <Pelicula/> {/* es un componente  */}
-      </main>
+    <div className="App"  id="wrapper">
+      <Sidebar/>
     </div>
   );
 }
