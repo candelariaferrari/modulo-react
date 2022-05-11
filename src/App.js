b947e18f7f9b773343cf7e357f23642816f59e30
candelaria ferrari <ferrari.candelaria@gmail.com>
2022-05-11 19:25:09 -0300

    Modulo 44

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './assets/css/App.css';
 
 import Sidebar from './components/SideBar';
 import ContentWrapper from './components/ContentWrapper';
+import ContenedorPublicidad from './components/contenedorPublicidad';
 
 function App() {
   return (
     <div className="App"  id="wrapper">
       <Sidebar/>
       <ContentWrapper/>
+      <ContenedorPublicidad>
+        <h2>publicidad 1 que puede ser una img </h2>
+      </ContenedorPublicidad>
+      <ContenedorPublicidad>
+        <h2>publicidad 2 que puede ser una video</h2>
+      </ContenedorPublicidad>
     </div>
   );
 }
